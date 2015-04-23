add28389d8b427f9870db152477ab1077a2e737d
aditya <adityaf.putra@gmail.com>
2015-04-23 17:22:01 +0700

    finished basic funtionalities

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function changeTo(color){
+	document.body.style.background = color;
+	if(color == 'black'){
+		document.body.style.color = 'white';
+	}
+	else {
+		document.body.style.color = 'black';
+	}
+}
