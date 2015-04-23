7cfa4406d516d7ef43d3040192047c69cd65188e
aditya <adityaf.putra@gmail.com>
2015-04-23 18:11:30 +0700

    minor changes to page title

--- deadpixeltest/README.md
+++
@@ -1,3 +0,0 @@
-# html-dead-pixel-check
-A simple HTML, CSS, and JS tool to check any dead pixels on your screen.
-<br/>Demo: http://adityaputra.com/misc/deadpixeltest/
--- deadpixeltest/scripts.js
+++
@@ -1,9 +0,0 @@
-function changeTo(color){
-	document.body.style.background = color;
-	if(color == 'black'){
-		document.body.style.color = 'white';
-	}
-	else {
-		document.body.style.color = 'black';
-	}
-}
